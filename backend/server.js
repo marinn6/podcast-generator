b07da113bf22e63ff9903c22620dc4ecfe9af294
marinn6 <nmarin@pursuit.org>
2025-01-23 13:52:48 -0500

    Created app.js file

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+const app = require("./app") //this line is importing express and cors.
 require("dontenv").config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Backend is running")
-})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
