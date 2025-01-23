b8ec426f975d203dade94da54249aa1f19a4ff29
marinn6 <nmarin@pursuit.org>
2025-01-23 15:32:31 -0500

    feat(api): add google gemini

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
-const app = require("./app") //this line is importing express and cors.
-require("dontenv").config();
+// const app = require("./app") //this line is importing express and cors.
+require("dotenv").config();
+const Google_api = process.env.google_api
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const genAI = new GoogleGenerativeAI(Google_api);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Explain how AI works";
+
+const test = async () => {
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+}
+
+test()
+
+
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
